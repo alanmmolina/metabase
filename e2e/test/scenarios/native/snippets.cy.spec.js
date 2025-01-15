e0e07a253fa25d20989ac6c14600c7b8dc8c3c5c
Nick Fitzpatrick <nickfitz.582@gmail.com>
2025-01-15 15:38:07 -0400

    more revisions

--- e2e/test/scenarios/native/snippets.cy.spec.js
+++ e2e/test/scenarios/native/snippets.cy.spec.js
@@ -1,5 +1,6 @@
 import { H } from "e2e/support";
 import { USER_GROUPS } from "e2e/support/cypress_data";
+import { modal } from "e2e/support/helpers";
 
 const { ALL_USERS_GROUP } = USER_GROUPS;
 
@@ -343,9 +344,11 @@ H.describeEE("scenarios > question > snippets (EE)", () => {
       H.popover().findByText("Edit folder details").click();
       H.modal().findByTestId("collection-picker-button").click();
 
-      H.entityPickerModal()
-        .findByRole("button", { name: /Snippet Folder/ })
-        .should("be.disabled");
+      H.entityPickerModalItem(1, /Snippet Folder/).should(
+        "have.attr",
+        "data-disabled",
+        "true",
+      );
     });
 
     it("should not display snippet folder as part of collections (metabase#14907)", () => {
@@ -383,7 +386,7 @@ H.describeEE("scenarios > question > snippets (EE)", () => {
       });
 
       H.popover().contains("View").click();
-      cy.button("Save").click();
+      modal().button("Save").click();
 
       cy.wait("@updatePermissions");
 
