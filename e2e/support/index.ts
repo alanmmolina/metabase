b37f4983a21c51eb5ee49a3344a6172b10951bb3
Filip Hric <filip@filiphric.sk>
2025-01-21 20:37:43 +0100

    introduce logGroup to handle helpers properly

--- e2e/support/index.ts
+++ e2e/support/index.ts
@@ -1,4 +1,47 @@
-// H is for helpers 🤗
-import * as H from "./helpers";
+import * as OriginalHelpers from "./helpers";
+import { logGroup } from "./logGroup";
 
-export { H };
+const formatArgs = (args: any[]): string[] => {
+  try {
+    if (args.length === 0) {
+      return [""];
+    }
+    return args.map(arg => {
+      if (arg === undefined) {
+        return "undefined";
+      }
+      if (arg === null) {
+        return "null";
+      }
+      if (typeof arg === "object") {
+        try {
+          return JSON.stringify(arg, null, 1);
+        } catch {
+          return "[Complex Object]";
+        }
+      }
+      return String(arg);
+    });
+  } catch (e) {
+    return ["Unable to stringify args"];
+  }
+};
+
+const wrappedHelpers = Object.entries(OriginalHelpers).reduce<
+  Record<string, any>
+>((acc, [key, helper]) => {
+  acc[key] = (...args: any[]) => {
+    return logGroup(
+      cy,
+      {
+        name: key,
+        displayName: `H.${key}`,
+        message: formatArgs(args),
+      },
+      () => helper(...args),
+    );
+  };
+  return acc;
+}, {});
+
+export const H = wrappedHelpers;
